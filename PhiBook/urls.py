61afedf741d6d5b17bb3adcdb8627196a5372b9a
mdeibrahim <mmeibrahim505@gmail.com>
2025-09-12 10:25:22 +0600

    add url for profile picture

--- PhiBook/urls.py
+++ PhiBook/urls.py
@@ -15,4 +15,10 @@ urlpatterns = [
     path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/v1/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/v1/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
 ]
+
+# Serve media files in development
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns += static(settings.MEDIA_URL, document_root=getattr(settings, 'MEDIA_ROOT', None))
